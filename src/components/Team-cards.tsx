c6fc9dbbff465c30fd3bf7bdd6c3501cb3788e61
Manon <manonbrochet789@gmail.com>
2025-12-09 08:04:34 +0100

    V2

--- src/components/Team-cards.tsx
+++ src/components/Team-cards.tsx
@@ -2,24 +2,51 @@ import React, { useState } from "react";
 import POKEMONS from "../models/mock-pokemon";
 
 import Pokemon from "../models/pokemon";
+import { ImCross } from "react-icons/im";
+import { CiLink } from "react-icons/ci";
 
 export default function TeamCards() {
     const [pokemons, setPokemons] = useState<Pokemon[]>(POKEMONS);
-   
+
+    function getColor(type: string): string {
+        if (type === "Feu") return "bg-red-400";
+        if (type === "Eau") return "bg-blue-400";
+        if (type === "Plante") return "bg-green-400";
+        if (type === "Electrik") return "bg-yellow-300";
+        if (type === "Poison") return "bg-purple-400";
+        if (type === "Insecte") return "bg-lime-400";
+        if (type === "Vol") return "bg-sky-300";
+        if (type === "Roche") return "bg-stone-400";
+        if (type === "Sol") return "bg-amber-600";
+        if (type === "Glace") return "bg-cyan-300";
+        if (type === "Dragon") return "bg-indigo-500";
+        if (type === "Ténèbres") return "bg-gray-700";
+        if (type === "Acier") return "bg-slate-400";
+        if (type === "Fée") return "bg-rose-300";
+        else return "bg-gray-300";
+    }
+
+    function deletePokemon(id: number) {
+        setPokemons(pokemons.filter(p => p.id !== id));
+    }
+
     return (
         <div className="grid justify-center grid-cols-3 gap-5">
             {pokemons.map((pokemon) =>
-            <div key={pokemon.id}>
-                if ({pokemon.types[0] == "Plante"}){
-                    <div className="shadow-md size-60 p-7 bg-green-400">
-                        <img src={pokemon.picture} className="w-48 h-auto" /> 
+                <div key={pokemon.id}>
+                    <div className={`shadow-md size-72 p-5 ${getColor(pokemon.types[0])}`}>
+                        <img src={pokemon.picture} className="w-48 center h-auto" />
                         <h1 className="text-sm text-center font-bold text-black px-1 py-1 rounded-lg">
                             {pokemon.name}
+                            <h2 className="text-xs text-center font-bold text-black px-1 py-1 rounded-lg">
+                                {pokemon.types.join(", ")}
+                            </h2>
+                            <ImCross className="place-self-center" onClick={() => deletePokemon(pokemon.id)} />
+                            <CiLink className="place-self-center" />
                         </h1>
+                    </div>
                 </div>
-                }
-            </div>
-        )}
+            )}
         </div>
     );
 }
--- src/pages/PokemonList.tsx
+++ src/pages/PokemonList.tsx
@@ -1,44 +1,28 @@
+import Pokemon from "../models/pokemon";
+import POKEMONS from "../models/mock-pokemon";
+import deletePokemon from "../components/Team-cards";
+
 function PokemonList() {
     return (
-        <div className="grid justify-center grid-cols-3 gap-5">
-            <div className="shadow-md size-60 p-7">
-                <img src="/pikachu.jpg" alt="pokemon pikachu" className="w-48 h-auto" />
-                <h1 className="text-sm text-center font-bold text-black px-1 py-1 rounded-lg">
-                    Pikachu
-                </h1>
-            </div>
-            <div className="shadow-md size-60 p-7">
-                <img src="/rattata.jpg" alt="pokemon rattata" className="w-48 h-auto" />
-                <h1 className="text-sm text-center font-bold text-black px-2 py-2 rounded-lg">
-                    Rattata
-                </h1>
-            </div><div className="shadow-md size-60 p-7">
-                <img src="/roucool.jpg" alt="pokemon roucool" className="w-48 h-auto" />
-                <h1 className="text-sm text-center font-bold text-black px-2 py-2 rounded-lg">
-                    Roucool
-                </h1>
-            </div>
-            <div className="shadow-md size-60 p-10">
-                <img src="/salameche.jpg" alt="pokemon salameche" className="w-48 h-auto" />
-                <h1 className="text-sm text-center font-bold text-black px-2 py-2 rounded-lg">
-                    Salamèche
-                </h1>
-            </div>
-            <div className="shadow-md size-60 p-7">
-                <img src="/carapuce.jpg" alt="pokemon carapuce" className="w-48 h-auto" />
-                <h1 className="text-sm text-center font-bold text-black px-2 py-2 rounded-lg">
-                    Carapuce
-                </h1>
-            </div>
-            <div className="shadow-md size-60 p-7">
-                <img src="/bulbizarre.jpg" alt="pokemon bulbizarre" className="w-48 h-auto" />
-                <h1 className="text-sm text-center font-bold text-black px-2 py-2 rounded-lg">
-                    Bulbizarre
-                </h1>
-            </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {POKEMONS.map((pokemon) => (
+                <PokeCard
+                    key={pokemon.id}
+                    pokemon={pokemon}
+                    borderColor="#5219a8ff"
+                    deletePokemon={deletePokemon}
+                />
+            ))}
         </div>
-
-
     )
 }
-export default PokemonList;
+
+type Props = {
+    pokemon: Pokemon;
+    borderColor?: string; // Ajout d'une prop optionnelle pour la couleur de la bordure
+    deletePokemon: (id: number) => void;
+}
+
+export default function PokeCard({pokemon, borderColor="#948c8cff", deletePokemon }: Props) {
+    style={{ borderColor: borderColor }}
+}
